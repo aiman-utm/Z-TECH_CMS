f95ca3d328336a0ea9b89643c93d2e78ca1ae4ef
Lee Jia Yee <yee00@graduate.utm.my>
2023-01-03 12:29:39 +0800

    lee updatedpart_logo

--- config/settings.php
+++ config/settings.php
@@ -3,7 +3,7 @@
 return [
     // 'analytics_id'     => null,
     'analytics_id'     => 'UA-252902653-1',
-    'logo'             => 'i/icons/android-chrome-192x192.png',
+    'logo'             => 'http://cdn.onlinewebfonts.com/svg/img_388047.png',
     'login_image'      => 'https://ssl.gstatic.com/accounts/ui/avatar_2x.png',
     'site_description' => 'Content Management System ; Create, Manage, And Modify Content',
     'site_title'       => 'Z-TECH'
