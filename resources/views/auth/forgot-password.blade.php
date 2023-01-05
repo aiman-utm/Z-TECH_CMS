ecd6e896f0ee94d8c04f57968ae6303c8242d6ad
ChangMinXuan <mxchang01@gmail.com>
2023-01-05 12:55:26 +0800

    5/1 cmx

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+
+@section('content')
+    <section class="hero is-light is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                <h3 class="title has-text-grey">{{ __('Change Password') }}</h3>
+                    <div class="box">
+                        <div class="mb-5">
+                            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                        </div>
+                        
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @elseif (session('error'))
+                            <div class="alert alert-danger" role="alert">
+                                {{ session('error') }}
+                            </div>
+                        @endif
+
+                        <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
+
+                            <div class="mb-5">
+                                <input name="email" :value="old('email')" type="email" class="input is-large @error('email') is-invalid @enderror" id="email" placeholder="Email" required autofocus >
+                                @error('email')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="control">
+                                <!-- <input type="hidden" name="_token" id="csrf-token" value="{{ csrf_token() }}" /> -->
+                                <button type="submit" class="button is-info is-fullwidth is-large">{{ __('Email Password Reset Link') }}</button>
+                                <!-- <button class="btn btn-success">Submit</button> -->
+                            </div>
+                        </form>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
