a4c111cea17679beeee1f0fff7c2a76a1cc057c8
ChangMinXuan <mxchang01@gmail.com>
2023-01-24 10:37:53 +0800

    Mx_update_24/1

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    // 'analytics_id'     => null,
     'analytics_id'     => 'UA-252902653-1',
     'logo'             => 'http://cdn.onlinewebfonts.com/svg/img_388047.png',
     'login_image'      => 'https://ssl.gstatic.com/accounts/ui/avatar_2x.png',
